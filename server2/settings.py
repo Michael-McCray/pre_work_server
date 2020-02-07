1c8b3a4817af15bf3b762b1a55e8585dc38adf88
TKH <tkh@TKHs-Air-2.fios-router.home>
2020-02-07 14:26:35 -0500

    new

--- server2/settings.py
+++ server2/settings.py
@@ -121,6 +121,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+GRAPHENE = {
+    'SCHEMA': 'server2.schema.schema'
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
